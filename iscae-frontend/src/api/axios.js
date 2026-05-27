28f83ad589ce58be2df77b610faefd4640961260
Lwelly <elwelij100@gmail.com>
2026-05-27 19:37:38 +0000

    fix api v1 routes

--- iscae-frontend/src/api/axios.js
+++ iscae-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://iscae-back.onrender.com/api',
+  baseURL: 'https://iscae-back.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
     'Accept':       'application/json',
